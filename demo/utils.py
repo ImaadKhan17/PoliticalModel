6cc3a14dbd284b838270d8c139ccda5c1429a4fa
Imaad Khan <imaadmois@gmail.com>
2025-08-04 15:42:31 -0700

    Fix filepaths

--- demo/utils.py
+++ demo/utils.py
@@ -5,10 +5,15 @@ from transformers import AutoTokenizer
 import pickle
 import json
 import gdown
+import os
 
-url = "https://drive.google.com/uc?id=1k0RxLq33219ElJvv8D63Ee3kUwbzRzpt"
-output = 'model.pth'
-gdown.download(url, output, quiet=False)
+
+BASE_DIR = os.path.dirname(__file__)
+
+model_path = os.path.join(BASE_DIR, 'model.pth')
+if not os.path.exists(model_path):
+    url = "https://drive.google.com/uc?id=1k0RxLq33219ElJvv8D63Ee3kUwbzRzpt"
+    gdown.download(url, model_path, quiet=False)
 
 model_name = "allenai/longformer-base-4096"
 num_classes = 187
@@ -16,20 +21,19 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 max_length = 1028
 device = "cpu"
 
-with open("mappings.json", "r") as file:
+mappings_path = os.path.join(BASE_DIR, "mappings.json")
+with open(mappings_path, "r") as file:
     mappings = json.load(file)
 
-
-with open('label_encoder.pkl', 'rb') as f:
+label_encoder_path = os.path.join(BASE_DIR, "label_encoder.pkl")
+with open(label_encoder_path, 'rb') as f:
     le = pickle.load(f)
-
-
 
 @st.cache_resource
 def load_model():
     model = PolitcalModel(model_name, num_classes).to(device)
     model.load_state_dict(
-    torch.load("model.pth", map_location="cpu", weights_only=False)
+        torch.load(model_path, map_location="cpu", weights_only=False)
     )
     model.eval()
     return model
@@ -42,24 +46,22 @@ def map_preds(preds):
 
     for i in mappings:
         sub_keys = mappings[i]
-        if label in sub_keys['Subtopics'].keys():
+        if label in sub_keys['Subtopics']:
             major = sub_keys['Major']
             minor = sub_keys['Subtopics'][label]
             break
-    
-    if stance >=-0.167 and stance <= 0.025:
+
+    if -0.167 <= stance <= 0.025:
         leaning = "Centrist"
-    elif stance <-0.167:
+    elif stance < -0.167:
         leaning = "Liberal"
     else:
         leaning = "Conservative"
 
     return major, minor, leaning, stance
-    
-
 
 def predict(text):
-    encoded  = tokenizer(text, padding="max_length", truncation=True, max_length=max_length, return_tensors='pt' )
+    encoded = tokenizer(text, padding="max_length", truncation=True, max_length=max_length, return_tensors='pt')
     input_ids = encoded['input_ids'].to(device)
     attention_mask = encoded['attention_mask'].to(device)
 
@@ -67,7 +69,6 @@ def predict(text):
         outputs = model(input_ids=input_ids, attention_mask=attention_mask)
     _, pred = torch.max(outputs[0], dim=1)
     stance = outputs[1].item()
-    label, stance = le.inverse_transform([pred.item()]), (stance*2)-1
-        
-    return map_preds((label[0], stance))
+    label, stance = le.inverse_transform([pred.item()]), (stance * 2) - 1
 
+    return map_preds((label[0], stance))
